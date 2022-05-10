ceb891839d249f0ebf4a0cf59bd31713686cd94f
somaggie <margaretsonora@gmail.com>
2022-05-10 11:49:55 -0500

    Update mini-calculator.py
    Made the formatting pretty and allowed the user to use the calculator more than once

--- mini-calculator.py
+++ mini-calculator.py
@@ -1,31 +1,27 @@
 print("Welcome to the mini calcuator! Numbers are processed in the same order they are input. For example a - b, a / b, and so on. Please input your numbers accordingly.")
-    num1 = float(input("Enter the first number"))
-    num2 = float(input("Enter the second number"))
-    operation = input("Enter + to add, - to subtract, * to multiply, or / to divide")
+userMenuInput = "n"
+while userMenuInput != "q" and userMenuInput != "Q":
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operation = input("Enter + to add, - to subtract, * to multiply, or / to divide: ")
     if operation == "+":
         print(num1 + num2)
-        break
     elif operation == "-":
         print(num1 - num2)
-        break
     elif operation == "*":
         print(num1 * num2)
-        break
     elif operation == "/":
         if num2 != 0:
             print(num1 / num2)
-            break
         elif num1 != 0:
             print("cannot divide num1 by num2 due to division by zero error. Num2 divide by num 1 is: ")
             print(num2 / num1)
-            break
         else:
             print("cannot divide by zero!!")
-            break
     else:
          print("Invalid operation input. Try again next time")
-         break
-    print("Thanks for using the mini calculator")
+    userMenuInput = input("Press q to quit or any key to use again: ")
 
+print("Thanks for using the mini calculator")
         
     
